c972798572ba7c141d5ba741df3f340a9daa07a9
Ivan Ciric <info@yoshi.tech>
2024-03-06 01:03:07 +0100

    cors

--- api/get-image.js
+++ api/get-image.js
@@ -8,6 +8,15 @@ const openai = new OpenAI({
 });
 
 export default async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method === 'POST') {
         try {
             const response = await openai.images.generate({
@@ -29,3 +38,5 @@ export default async (req, res) => {
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 };
+
+module.exports = app;
--- api/mint.js
+++ api/mint.js
@@ -10,6 +10,15 @@ const minter = network == 'testnet' ? process.env.MINTER_TESTNET : process.env.M
 const mintbaseWalletUrl = network == 'testnet' ? process.env.MINTBASE_WALLET_TESTNET : process.env.MINTBASE_WALLET_MAINNET;
 
 export default async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method === 'POST') {
         try {
             let { imageUrl, name, description, redirectUrl } = req.body;
