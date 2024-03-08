7d5128c87d1b74c019ca9aaf4c6dba9b89ea7f1f
Ivan Ciric <info@yoshi.tech>
2024-03-08 23:16:30 +0100

    getting token metadata from id

--- api/get-token-link.js
+++ api/get-token-link.js
@@ -23,8 +23,8 @@ export default async (req, res) => {
             let metadata = await getMetadataIdFromTokenId(tokenId); 
             console.log(metadata.data.nft_tokens[0].metadata_id)
             let metadataId = metadata.data.nft_tokens[0].metadata_id;
-            let url = `${mintbaseBaseUrl}/meta/${nftContract}%3A${metadataId}`;
-            res.status(200).json({ nftUrl: url });
+            let url = `${mintbaseBaseUrl}/meta/${metadataId}`;
+            res.status(200).json({ nftUrl: encodeURIComponent(url) });
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Error in minting process' });
