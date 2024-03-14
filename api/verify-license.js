2435242517c597c71099599508a2164fcf138951
Ivan Ciric <info@yoshi.tech>
2024-03-14 22:18:44 +0100

    adds license verification

--- api/verify-license.js
+++ api/verify-license.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
     initializeApp({
       credential: cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
-          appId: process.env.FIREBASE_APP_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
           privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       }),
   });
